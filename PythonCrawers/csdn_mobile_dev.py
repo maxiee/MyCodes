97039ccff1c99d037b5d77b113a4a5f95214ab01
Maxiee <maxieewong@gmail.com>
2014-11-17 16:41:41 +0800

    csdn_mobile_dev.py:CSDN blog mobile dev sub catagory crawer

--- PythonCrawers/csdn_mobile_dev.py
+++ PythonCrawers/csdn_mobile_dev.py
@@ -1,12 +1,11 @@
-__author__ = 'maxiee'
+__author__ = 'Maxiee'
 # -*- coding: UTF-8 -*-
 
 import mechanize
 from bs4 import BeautifulSoup
-import time
 
 '''
-程序功能：查看CSDN热门博客前10页
+程序功能：查看CSDN博客移动开发分类
 '''
 
 #设置Python编码，否则不能创建文件
@@ -14,6 +13,7 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+# 浏览器伪装
 br = mechanize.Browser()
 br.set_handle_equiv(True)
 br.set_handle_redirect(True)
@@ -22,13 +22,11 @@ br.set_handle_robots(False)
 br.addheaders = [('User-agent',
                   'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
-# file = open(u'[CSDN热门TOP10]['+time.strftime(u'%Y年%m月%d日')+u'].txt','a')
-
-for page in range(1, 2):
-    url = 'http://blog.csdn.net/hot.html?page=' + str(page)
+for page in range(1, 10):
+    url = 'http://blog.csdn.net/mobile/index.html?&page=' + str(page)
     response = br.open(url)
     soup = BeautifulSoup(response)
-    soup = soup.find('div', 'page_right')
+
 
     # file.write("正在加载第%d页:\r\n\r\n" % (page))
     print("正在加载第%d页:\n" % (page))
