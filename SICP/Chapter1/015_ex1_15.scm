0751d1f07bc98b0c8e329478584992657a8d4263
Maxiee <maxieewong@gmail.com>
2014-12-19 23:20:41 +0800

    015_ex1_15.scm 016_sum.scm

---
+++ SICP/Chapter1/015_ex1_15.scm
@@ -0,0 +1,8 @@
+(define (cube x) (* x x x))
+
+(define (p x) (- (* 3 x) (* 4 (cube x))))
+
+(define (sine angle)
+    (if (not (> (abs angle) 0.1))
+	    angle
+		(p (sine (/ angle 3.0)))))
---
+++ SICP/Chapter1/016_sum.scm
@@ -0,0 +1,5 @@
+(define (sum term a next b)
+    (if (> a b)
+        0
+        (+ (term a)
+            (sum term (next a) next b))))
