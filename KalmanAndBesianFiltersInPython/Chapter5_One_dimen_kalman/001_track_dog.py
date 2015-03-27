9e03d605117b39b8210eff60d13d7beb4b06420a
Maxiee <maxieewong@gmail.com>
2015-03-27 11:44:57 +0800

    track dog using Kalman filter.

--- KalmanAndBesianFiltersInPython/Chapter5_One_dimen_kalman/001_track_dog.py
+++ KalmanAndBesianFiltersInPython/Chapter5_One_dimen_kalman/001_track_dog.py
@@ -1,6 +1,40 @@
 import matplotlib.pyplot as plt
 import numpy.random as random
 import math
+import numpy as np
+import scipy.stats as stats
+
+class KalmanFilter1D:
+    def __init__(self, x0, P, R, Q):
+        self.x = x0 # esimated value of the filter
+        self.P = P  # variance of the state
+        self.R = R  # measurement error
+        self.Q = Q  # movement error
+    
+    def update(self, z): # just multiply
+        self.x = (self.P*z + self.x*self.R) / (self.P + self.R)
+        self.P = 1. / (1./self.P + 1./self.R)
+    
+    def predict(self, u=0.0): # jsut predict
+        self.x += u
+        self.P += self.Q
+
+
+def multiply(mu1, var1, mu2, var2):
+    if var1 == 0:
+        var1 = 1e-80
+    
+    if var2 == 0:
+        var2 = 1e-80
+    mean = (var1*mu2 + var2*mu1) / (var1+var2)
+    variance = 1 / (1/var1 + 1/var2)
+    return (mean, variance)
+
+def update(mean, variance, measurement, measurement_variance):
+    return multiply(mean, variance, measurement, measurement_variance)
+
+def predict(pos, variance, movement, movement_variance):
+    return (pos + movement, variance + movement_variance)
 
 class DogSensor(object):
     
@@ -22,7 +56,32 @@ class DogSensor(object):
         self.x = self.x + self.velocity
         return self.x + random.randn()*self.noise
 
-for i in range(20):
-    print '{: 5.4f}'.format(random.randn())
-    if (i+1) % 5 == 0:
-        print ""
+
+movement = 1
+movement_variance = 2.0
+sensor_variance = 4.5
+pos = (0, 100)
+
+dog = DogSensor(pos[0],
+                velocity=movement,
+                measurement_variance=sensor_variance,
+                process_variance=sensor_variance)
+
+zs = []
+ps = []
+
+for i in range(50):
+    pos = predict(pos[0], pos[1], movement, movement_variance)
+    print "PRE:{: 10.4f} {: 10.4f}".format(pos[0],pos[1]) ,
+    
+    z = dog.sense_position()
+    zs.append(z)
+    
+    pos = update(pos[0], pos[1], z, sensor_variance)
+    ps.append(pos[0])
+    print "UPD:{: 10.4f} {: 10.4f}".format(pos[0],pos[1])
+
+plt.plot(ps)
+plt.plot(zs)
+plt.legend(["filter", "measurement"], loc="best")
+plt.show()
