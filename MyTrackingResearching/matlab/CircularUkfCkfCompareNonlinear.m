7073f82b2992bc4fd51eb2a02d48684c7988e7f5
Maxiee <maxieewong@gmail.com>
2015-04-01 17:18:36 +0800

    still something wrong in the filter.

--- MyTrackingResearching/matlab/CircularUkfCkfCompareNonlinear.m
+++ MyTrackingResearching/matlab/CircularUkfCkfCompareNonlinear.m
@@ -8,15 +8,15 @@ h = @(x, params) [x(1,:);x(3,:)];
 
 dt = 1;
 radius = 100;
-delta = 2*pi/360*6;
-count = 70;
+delta = 2*pi/360*5.73;
+count = 62;
 
 txs = zeros(2, count);
 uxs = zeros(5, count);
 cxs = zeros(5, count);
 
 x0 = [100 0 0 10 0.1]';
-P0 = diag([100 10 100 10 1]);
+P0 = diag([100 10 100 10 10]);
 
 M_ukf = x0;
 P_ukf = P0;
@@ -27,11 +27,11 @@ Qv = [0.1/3     0.1/2   0         0         0;
       0.1/2     0.1     0         0         0;
       0         0       0.1/3     0.1/2     0;
       0         0       0.1/2     0.1       0;
-      0         0       0         0         3];
+      0         0       0         0         1.75];
 
 Qw = diag([25 25]);
 
-for i=1:70
+for i=1:count
     fprintf('==================第%d轮======================\n',i);
     
     target_pos_x = cos(i*delta)*radius + randn(1)*0.0001;
@@ -41,7 +41,7 @@ for i=1:70
     zx = target_pos_x + randn(1)*5;
     zy = target_pos_y + randn(1)*5;
     
-    [M_ukf, P_ukf,D,upSX,upSY] = ukf_predict1(M_ukf, P_ukf, f, Qv, dt);
+    [M_ukf, P_ukf,D,upSX,upSY] = ukf_predict1(M_ukf, P_ukf, f, Qv, dt,1,1,-2);
     [M_ukf, P_ukf,uK,uMU,uS,uLH,uuSx,uuSY] = ukf_update1(M_ukf, P_ukf, [zx; zy], h, Qw);
     
     [M_ckf, P_ckf,cpSX,cpSY] = ckf_predict(M_ckf, P_ckf, f, Qv, dt);
@@ -49,7 +49,7 @@ for i=1:70
     
 %     fprintf('predict 变换前 Sigma/Cubature 点比较：\n');
 %     upSX
-%     uuSx
+%     cpSX
 %     fprintf('predict 变换后 Sigma/Cubature 点比较：\n');
 %     upSY
 %     cpSY
@@ -59,7 +59,11 @@ for i=1:70
 %     fprintf('update 变换后 Sigma/Cubature 点比较：\n');
 %     uuSY
 %     cuSY
-%     
+
+    fprintf('CKF卡尔曼增益：');
+    cK
+    
+    fprintf('目标真实位置:(%f,%f)\n',zx,zy);
     fprintf('UKF CKF 结果输出：\n');
     M_ukf
     M_ckf
