cf404f989c8bd7d6d521f32453dfb192ec563b8b
maxiee <maxieewong@gmail.com>
2016-07-04 10:39:40 +0800

    BinarySearch.java: add a method for exercise.

--- Algorithms/src/com/maxiee/algorithms/BinarySearch.java
+++ Algorithms/src/com/maxiee/algorithms/BinarySearch.java
@@ -11,14 +11,13 @@ import java.util.Arrays;
  */
 public class BinarySearch {
 
-    private final static String TINY_T = "/home/maxiee/Code/algorithmsResources/tinyT.txt";
-    private final static String TINY_W = "/home/maxiee/Code/algorithmsResources/tinyW.txt";
+    private final static String TINY_T = "./tinyT.txt";
+    private final static String TINY_W = "./tinyW.txt";
 
-    public static int rank(int key, int[] a) {
+    public static int rank_correct(int key, int[] a) {
         int lo = 0;
         int hi = a.length - 1;
         while (lo <= hi) {
-//            System.out.print(".");
             int mid = lo + (hi - lo) / 2;
             if (a[mid] < key) { lo = mid + 1;}
             else if (a[mid] > key) {hi = mid - 1;}
@@ -47,16 +46,30 @@ public class BinarySearch {
 
     public static void main(String[] args) {
         try {
-            int[] tinyW = fileToIntArray(TINY_W);
-            int[] tinyT = fileToIntArray(TINY_T);
+            int[] tinyW = fileToIntArray(TINY_W); // 要寻找的数字
+            int[] tinyT = fileToIntArray(TINY_T); // 从这个数组中搜索
             System.out.println("数组排序...");
-            Arrays.sort(tinyT);
+            Arrays.sort(tinyT); // 二分查找必须先排序
             System.out.println("开始二值查找...");
             for (int value: tinyW) {
-                int result = rank(value, tinyT);
-                System.out.println("当前值:" + String.valueOf(value) + ", 位于:" + String.valueOf(result));
+                int result = rank_correct(value, tinyT); // rank 就是二分查找的函数
+                System.out.println("当前值:" + String.valueOf(value) + ", 正确位置位于:" + String.valueOf(result));
+                result = rank_my(value, tinyT);
+                System.out.println("当前值:" + String.valueOf(value) + ", 算法位置位于:" + String.valueOf(result));
             }
         } catch (IOException e) {e.printStackTrace();}
+    }
+
+    // 每次要练算法的时候, 就把这个函数清空, 重新编写.
+    private static int rank_my(int k, int[] a) {
+        int lo = 0, hi = a.length - 1;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (a[mid] < k) lo = mid + 1;
+            else if (a[mid] > k) hi = mid -1;
+            else return mid;
+        }
+        return -1;
     }
 
 }
