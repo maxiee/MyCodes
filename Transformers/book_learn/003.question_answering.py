13aa58e443ebeb76c01277b4ecaaed20d7bd0539
Maxiee <maxieewong@gmail.com>
2023-07-04 00:12:27 +0800

    003.question_answering.py

---
+++ Transformers/book_learn/003.question_answering.py
@@ -0,0 +1,14 @@
+from transformers import pipeline
+import pandas as pd
+
+text = "In this chapter, we will first start with the background of NLP that led to the rise of the Transformer. We will briefly go from early NLP to RNNs and CNNs. Then we will see how the Transformer overthrew the reign of RNNs and CNNs, which had prevailed for decades for sequence analysis."
+
+reader = pipeline("question-answering")
+
+question = "What is the chapter about?"
+
+outputs = reader(question=question, context=text)
+
+outputs = pd.DataFrame([outputs])
+
+print(outputs.to_markdown())
