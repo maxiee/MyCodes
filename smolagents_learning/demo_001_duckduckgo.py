b5212fee4807c3adece60d0bdfc7b79fde95a43b
Maxiee <maxieewong@gmail.com>
2025-02-10 20:00:56 +0800

    Refactor demo script to use HfApiModel and update environment variable names for model configuration

--- smolagents_learning/demo_001_duckduckgo.py
+++ smolagents_learning/demo_001_duckduckgo.py
@@ -1,15 +1,14 @@
-from smolagents import CodeAgent, DuckDuckGoSearchTool, OpenAIServerModel
+from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-model_id = os.getenv('siliconflow_model_Qwen2.5-Coder-7B-Instruct')
-api_base = os.getenv('siliconflow_api_base')
-api_key = os.getenv('siliconflow_key')
+model_id = os.getenv('model_id')
+api_base = os.getenv('api_base')
+api_key = os.getenv('api_key')
 
-model = OpenAIServerModel(model_id="Qwen/Qwen2.5-7B-Instruct", api_base=api_base, api_key=api_key)
+model = HfApiModel(model_id=model_id, api_base=api_base, api_key=api_key)
 agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=model)
 
-agent.write_memory_to_messages()
-agent.run('什么是大模型?')
+agent.run('How to use LLM for reading source code?')
