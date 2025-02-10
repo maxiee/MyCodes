6ef6655196ac5b5e27a30fe28d6f6e452cf6317a
Maxiee <maxieewong@gmail.com>
2025-02-10 11:27:39 +0800

    Add .gitignore and update demo script to load environment variables for model configuration

--- smolagents_learning/demo_001_duckduckgo.py
+++ smolagents_learning/demo_001_duckduckgo.py
@@ -1,6 +1,14 @@
 from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel
+import os
+from dotenv import load_dotenv
 
-model = HfApiModel()
+load_dotenv()
+
+model_id = os.getenv('model_id')
+api_base = os.getenv('api_base')
+api_key = os.getenv('api_key')
+
+model = HfApiModel(model_id=model_id, api_base=api_base, api_key=api_key)
 agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=model)
 
 agent.run('How to use LLM for reading source code?')
