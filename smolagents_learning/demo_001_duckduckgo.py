5273fc7967f521a07822c0a197d9d2e29b0273a6
Maxiee <maxieewong@gmail.com>
2025-02-10 12:04:18 +0800

    Update dependencies and modify demo script to use OpenAIServerModel with new environment variables

--- smolagents_learning/demo_001_duckduckgo.py
+++ smolagents_learning/demo_001_duckduckgo.py
@@ -1,14 +1,15 @@
-from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel
+from smolagents import CodeAgent, DuckDuckGoSearchTool, OpenAIServerModel
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-model_id = os.getenv('model_id')
-api_base = os.getenv('api_base')
-api_key = os.getenv('api_key')
+model_id = os.getenv('siliconflow_model_Qwen2.5-Coder-7B-Instruct')
+api_base = os.getenv('siliconflow_api_base')
+api_key = os.getenv('siliconflow_key')
 
-model = HfApiModel(model_id=model_id, api_base=api_base, api_key=api_key)
+model = OpenAIServerModel(model_id="Qwen/Qwen2.5-7B-Instruct", api_base=api_base, api_key=api_key)
 agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=model)
 
+agent.write_memory_to_messages()
 agent.run('How to use LLM for reading source code?')
