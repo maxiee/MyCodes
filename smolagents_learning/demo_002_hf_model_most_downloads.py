85980fc5a9d695a7504b148ea0295ed45e5d90b3
Maxiee <maxieewong@gmail.com>
2025-02-10 13:21:14 +0800

    Update demo script to use environment variables for model ID and improve task descriptions

--- smolagents_learning/demo_002_hf_model_most_downloads.py
+++ smolagents_learning/demo_002_hf_model_most_downloads.py
@@ -4,19 +4,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-model_id = "Qwen/Qwen2.5-7B-Instruct"
+model_id = os.getenv("siliconflow_model_Qwen2.5-Coder-7B-Instruct") #"Qwen/Qwen2.5-7B-Instruct"
 api_base = os.getenv('siliconflow_api_base')
 api_key = os.getenv('siliconflow_key')
 
 class HFModelDownloadsTool(Tool):
     name = "model_download_counter"
     description = """
-    这是一个工具，传入指定任务，返回最多下载次数的模型的ID。
-    返回 checkpoint 的名称。"""
+    This is a tool that returns the most downloaded model of a given task on the Hugging Face Hub.
+    It returns the name of the checkpoint."""
     inputs = {
         "task": {
             "type": "string",
-            "description": "任务分类（例如 text-classification, depth-estimation)"
+            "description": "the task category (such as text-classification, depth-estimation, etc)",
         }
     }
     output_type = "string"
@@ -30,4 +30,4 @@ class HFModelDownloadsTool(Tool):
 model = OpenAIServerModel(model_id=model_id, api_base=api_base, api_key=api_key)
 agent = CodeAgent(tools=[HFModelDownloadsTool()], model=model)
 
-agent.run('下载最多的coder模型是什么？')
+agent.run('whaht is the most download model for chat?')
